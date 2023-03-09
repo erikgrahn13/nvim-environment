b2341fc5af0d9788c0f422882f54f4bfc201bcb0
Erik Grahn <erikgrahn13@gmail.com>
2023-03-09 21:57:30 +0100

    initial commit

---
+++ lua/user/options.lua
@@ -0,0 +1,22 @@
+-- :help options
+vim.opt.backup = false			-- create a backup file
+vim.opt.clipboard = "unnamedplus"	-- allows neovim to access the system clipboard
+vim.opt.fileencoding = "utf-8"		-- the encoding written to a file
+vim.opt.hlsearch = true			-- hightlight all matches on previous search pattern
+vim.opt.ignorecase = true		-- ignore case in search patterns
+vim.opt.mouse = "a"			-- allow the mouse to be used in neovim
+vim.opt.showtabline = 2			-- always show tabs
+vim.opt.guicursor = "n-v-c-sm:block,i-ci-ve:block-blinkwait175-blinkoff150-blinkon175,r-cr-o:hor20"		-- sett cursor to block style
+vim.opt.smartindent = true		-- make indenting smarter again
+vim.opt.smartcase = true		-- smart case for searching
+vim.opt.undofile = true			-- enable persistent undo
+vim.opt.expandtab = true		-- convert tabs to spaces
+vim.opt.shiftwidth = 2       -- the number of spaces inserted for each indentation
+vim.opt.tabstop = 2			-- insert 2 spaces for a tab
+vim.opt.number = true			-- set numbered lines
+vim.opt.relativenumber = true		-- set relative numbered lines
+vim.opt.splitbelow = true		-- force all horizontal split to go below current window
+vim.opt.splitright = true		-- force all vertical splits to go to the right of current window
+vim.opt.scrolloff = 8
+vim.opt.guifont = "monospace:h17"
+
